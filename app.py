01a0a8428d393a550972d147667c1e8b1606242d
Gorle <ngorle@netapp.com>
2026-06-30 21:19:35 +0530

    changed app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+
+
+AWS_REGION = "us-east-1"
+AMI_ID = "ami-08f44e8eca9095668"
+INSTANCE_TYPE = "t2.medium"
+KEY_PAIR_NAME = "nag-in"
+SECURITY_GROUP_ID = "sg-07341526e9e2540b3"
+SUBNET_ID = "subnet-0cd8d94cc14984f8b"
+SERVER_NAME = "boto3-server"
