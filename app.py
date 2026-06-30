017e41a764f827c25e64435d17b2b3a25c529662
Gorle <ngorle@netapp.com>
2026-06-30 21:25:46 +0530

    modified app.py

--- app.py
+++ app.py
@@ -8,3 +8,14 @@ KEY_PAIR_NAME = "nag-in"
 SECURITY_GROUP_ID = "sg-07341526e9e2540b3"
 SUBNET_ID = "subnet-0cd8d94cc14984f8b"
 SERVER_NAME = "boto3-server"
+
+def validate_network(ec2):
+	subnet = ec2.describe_subnets(SubnetIds=[SUBNET_ID])["Subnets"][0]
+	security_group = ec2.describe_security_groups(GroupIds=[SECURITY_GROUP_ID])["SecurityGroups"][0]
+
+	if subnet["VpcId"] != security_group["VpcId"]:
+		raise ValueError(
+			"Security group and subnet must be in the same VPC. "
+			f"Subnet {SUBNET_ID} is in {subnet['VpcId']}; "
+			f"security group {SECURITY_GROUP_ID} is in {security_group['VpcId']}."
+		)
